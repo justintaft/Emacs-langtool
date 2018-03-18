0ac57c10b06192d93621a419102c026e940d88ac
justin taft <justin@justins-MacBook-Pro.local>
2018-03-18 01:43:37 -0400

    Add grammar test

--- langtool-test.el
+++ langtool-test.el
@@ -1,2 +1,24 @@
-(require 'ert)
+(require 'ert-async)
 (require 'langtool)
+
+(defun langtooltest-grammar-check (callback)
+  (with-current-buffer (get-buffer-create "langtool-test")
+  (should (eq (get-char-property (point) 'face) 'langtool-errline))
+  (funcall callback)
+))
+
+(ert-deftest-async langtooltest-grammar-check (done-1)
+    "Ensure double works are highlighted."
+    (with-current-buffer (get-buffer-create "langtool-test")
+        (erase-buffer)
+	(insert "This is a a test")
+	(langtool-check)
+	(search-backward "a")
+	(message "%s" (get-char-property (point) 'face))
+	(run-at-time "1 sec" nil (lambda () (langtooltest-grammar-check done-1)))
+    )
+)
+
+   
+
+  
